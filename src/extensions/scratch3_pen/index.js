11c244083951ff32f5a39d1fb563f532f400bd18
familycomicsstudios <themadpunter10@gmail.com>
2023-01-11 23:05:56 +0000

    Better pen

--- src/extensions/scratch3_pen/index.js
+++ src/extensions/scratch3_pen/index.js
@@ -30,6 +30,16 @@ const ColorParam = {
 };
 
 /**
+ * Enum for layer parameter values.
+ * @readonly
+ * @enum {string}
+ */
+ const LayerParam = {
+    FRONT: 'front',
+    BACK: 'back'
+};
+
+/**
  * @typedef {object} PenState - the pen state associated with a particular target.
  * @property {Boolean} penDown - tracks whether the pen should draw for this target.
  * @property {number} color - the current color (hue) of the pen.
@@ -63,6 +73,19 @@ class Scratch3PenBlocks {
          * @private
          */
         this._penSkinId = -1;
+
+        /**
+         * The attribute of print text.
+         * @type {object}
+         */
+        this.printTextAttribute = {
+            bold: false,
+            underline: false,
+            italic: false,
+            size: '28',
+            font: 'Arial',
+            color: '#000000'
+        };
 
         this._onTargetCreated = this._onTargetCreated.bind(this);
         this._onTargetMoved = this._onTargetMoved.bind(this);
@@ -98,7 +121,7 @@ class Scratch3PenBlocks {
      * @type {{min: number, max: number}}
      */
     static get PEN_SIZE_RANGE () {
-        return {min: 1, max: 1200};
+        return {min: 1, max: 1e308};
     }
 
     /**
@@ -142,6 +165,14 @@ class Scratch3PenBlocks {
             this._penSkinId = this.runtime.renderer.createPenSkin();
             this._penDrawableId = this.runtime.renderer.createDrawable(StageLayering.PEN_LAYER);
             this.runtime.renderer.updateDrawableSkinId(this._penDrawableId, this._penSkinId);
+
+            this.bitmapCanvas = document.createElement('canvas');
+            this.bitmapCanvas.width = this.runtime.stageWidth;
+            this.bitmapCanvas.height = this.runtime.stageHeight;
+            this.bitmapSkinID = this.runtime.renderer.createBitmapSkin(this.bitmapCanvas, 1);
+            this.bitmapDrawableID = this.runtime.renderer.createDrawable(StageLayering.PEN_LAYER);
+            this.runtime.renderer.updateDrawableSkinId(this.bitmapDrawableID, this.bitmapSkinID);
+            this.runtime.renderer.updateDrawableVisible(this.bitmapDrawableID, false);
         }
         return this._penSkinId;
     }
@@ -252,6 +283,27 @@ class Scratch3PenBlocks {
         ];
     }
 
+    getLayerParam () {
+        return [
+            {
+                text: formatMessage({
+                    id: 'pen.layerMenu.front',
+                    default: 'front',
+                    description: 'label for front'
+                }),
+                value: LayerParam.FRONT
+            },
+            {
+                text: formatMessage({
+                    id: 'pen.layerMenu.back',
+                    default: 'back',
+                    description: 'label for back'
+                }),
+                value: LayerParam.BACK
+            }
+        ];
+    }
+
     /**
      * Clamp a pen color parameter to the range (0,100).
      * @param {number} value - the value to be clamped.
@@ -317,6 +369,103 @@ class Scratch3PenBlocks {
                         description: 'render current costume on the background'
                     }),
                     filter: [TargetType.SPRITE]
+                },
+                {
+                    opcode: 'setPrintFont',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.setPrintFont',
+                        default: 'set print font to [FONT]',
+                        description: 'set print font'
+                    }),
+                    arguments: {
+                        FONT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'Arial'
+                        }
+                    }
+                },
+                {
+                    opcode: 'setPrintFontSize',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.setPrintFontSize',
+                        default: 'set print font size to [SIZE]',
+                        description: 'set print font size'
+                    }),
+                    arguments: {
+                        SIZE: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 24
+                        }
+                    }
+                },
+                {
+                    opcode: 'setPrintFontColor',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.setPrintFontColor',
+                        default: 'set print font color to [COLOR]',
+                        description: 'set print font color'
+                    }),
+                    arguments: {
+                        COLOR: {
+                            type: ArgumentType.COLOR
+                        }
+                    }
+                },
+                {
+                    opcode: 'printText',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.printText',
+                        default: 'print [TEXT] on x:[X] y:[Y]',
+                        description: 'print text'
+                    }),
+                    arguments: {
+                        TEXT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'Foobars are yummy'
+                        },
+                        X: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        Y: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        }
+                    }
+                },
+                {
+                    opcode: 'drawRect',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.drawRect',
+                        default: 'use [COLOR] to draw a square on x:[X] y:[Y] width:[WIDTH] height:[HEIGHT]',
+                        description: 'draw a square'
+                    }),
+                    arguments: {
+                        COLOR: {
+                            type: ArgumentType.COLOR
+                        },
+                        X: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        Y: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        WIDTH: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 10
+                        },
+                        HEIGHT: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 10
+                        }
+                    }
                 },
                 {
                     opcode: 'penDown',
@@ -433,7 +582,7 @@ class Scratch3PenBlocks {
                     blockType: BlockType.COMMAND,
                     text: formatMessage({
                         id: 'pen.setShade',
-                        default: 'set pen shade to [SHADE]',
+                        default: 'LEGACY - set pen shade to [SHADE]',
                         description: 'legacy pen blocks - set pen shade'
                     }),
                     arguments: {
@@ -442,14 +591,14 @@ class Scratch3PenBlocks {
                             defaultValue: 1
                         }
                     },
-                    hideFromPalette: true
+                    hideFromPalette: false
                 },
                 {
                     opcode: 'changePenShadeBy',
                     blockType: BlockType.COMMAND,
                     text: formatMessage({
                         id: 'pen.changeShade',
-                        default: 'change pen shade by [SHADE]',
+                        default: 'LEGACY - change pen shade by [SHADE]',
                         description: 'legacy pen blocks - change pen shade'
                     }),
                     arguments: {
@@ -458,14 +607,14 @@ class Scratch3PenBlocks {
                             defaultValue: 1
                         }
                     },
-                    hideFromPalette: true
+                    hideFromPalette: false
                 },
                 {
                     opcode: 'setPenHueToNumber',
                     blockType: BlockType.COMMAND,
                     text: formatMessage({
                         id: 'pen.setHue',
-                        default: 'set pen color to [HUE]',
+                        default: 'LEGACY - set pen color to [HUE]',
                         description: 'legacy pen blocks - set pen color to number'
                     }),
                     arguments: {
@@ -474,14 +623,14 @@ class Scratch3PenBlocks {
                             defaultValue: 1
                         }
                     },
-                    hideFromPalette: true
+                    hideFromPalette: false
                 },
                 {
                     opcode: 'changePenHueBy',
                     blockType: BlockType.COMMAND,
                     text: formatMessage({
                         id: 'pen.changeHue',
-                        default: 'change pen color by [HUE]',
+                        default: 'LEGACY - change pen color by [HUE]',
                         description: 'legacy pen blocks - change pen color'
                     }),
                     arguments: {
@@ -490,13 +639,33 @@ class Scratch3PenBlocks {
                             defaultValue: 1
                         }
                     },
-                    hideFromPalette: true
+                    hideFromPalette: false
+                },
+                {
+                    opcode: 'goPenLayer',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'pen.GoPenLayer',
+                        default: 'go to [OPTION] layer',
+                        description: 'go to front layer(pen)'
+                    }),
+                    arguments: {
+                        OPTION: {
+                            type: ArgumentType.STRING,
+                            menu: 'layerParam',
+                            defaultValue: LayerParam.FRONT
+                        }
+                    },
                 }
             ],
             menus: {
                 colorParam: {
                     acceptReporters: true,
                     items: this._initColorParam()
+                },
+                layerParam: {
+                    acceptReporters: false,
+                    items: this.getLayerParam()
                 }
             }
         };
@@ -511,6 +680,71 @@ class Scratch3PenBlocks {
             this.runtime.renderer.penClear(penSkinId);
             this.runtime.requestRedraw();
         }
+    }
+
+    setPrintFont (args) {
+        this.printTextAttribute.font = args.FONT;
+    }
+    setPrintFontSize (args) {
+        this.printTextAttribute.size = args.SIZE;
+    }
+    setPrintFontColor (args) {
+        const rgb = Cast.toRgbColorObject(args.COLOR);
+        const hex = Color.rgbToHex(rgb);
+        this.printTextAttribute.color = hex;
+    }
+
+    printText (args, util) {
+        const penSkinId = this._getPenLayerID(); // 获取画笔图层ID
+
+        const width = util.target.runtime.constructor.STAGE_WIDTH;
+        const height = util.target.runtime.constructor.STAGE_HEIGHT;
+        const ctx = this.bitmapCanvas.getContext('2d');
+        ctx.clearRect(0, 0, width, height);
+        ctx.save();
+        ctx.translate(width / 2, height / 2);
+        let resultFont = '';
+        resultFont += `${this.printTextAttribute.size}px `;
+        resultFont += this.printTextAttribute.font;
+        ctx.font = resultFont;
+
+        ctx.strokeStyle = this.printTextAttribute.color;
+        ctx.fillStyle = ctx.strokeStyle;
+
+        ctx.fillText(args.TEXT, args.X, -args.Y);
+        ctx.restore();
+
+        const printSkin = util.target.runtime.renderer._allSkins[this.bitmapSkinID];
+        const imageData = ctx.getImageData(0, 0, width, height);
+        printSkin._setTexture(imageData);
+        this.runtime.renderer.penStamp(penSkinId, this.bitmapDrawableID);
+
+        this.runtime.requestRedraw();
+    }
+
+    drawRect (args, util) {
+        const penSkinId = this._getPenLayerID(); // 获取画笔图层ID
+
+        const width = util.target.runtime.constructor.STAGE_WIDTH;
+        const height = util.target.runtime.constructor.STAGE_HEIGHT;
+        const ctx = this.bitmapCanvas.getContext('2d');
+        ctx.clearRect(0, 0, width, height);
+        ctx.save();
+        ctx.translate(width / 2, height / 2);
+
+        const rgb = Cast.toRgbColorObject(args.COLOR);
+        const hex = Color.rgbToHex(rgb);
+        ctx.fillStyle = hex;
+        ctx.strokeStyle = ctx.fillStyle;
+        ctx.fillRect(args.X, -args.Y, args.WIDTH, args.HEIGHT);
+        ctx.restore();
+
+        const printSkin = util.target.runtime.renderer._allSkins[this.bitmapSkinID];
+        const imageData = ctx.getImageData(0, 0, width, height);
+        printSkin._setTexture(imageData);
+        this.runtime.renderer.penStamp(penSkinId, this.bitmapDrawableID);
+
+        this.runtime.requestRedraw();
     }
 
     /**
@@ -798,6 +1032,21 @@ class Scratch3PenBlocks {
 
         this._updatePenColor(penState);
     }
+
+    goPenLayer (args) {
+        if (this.runtime.renderer) {
+            if (args.OPTION === LayerParam.FRONT) {
+                console.log('setting the layer order to', StageLayering.LAYER_GROUPS_PEN)
+                this.runtime.renderer.setLayerGroupOrdering(StageLayering.LAYER_GROUPS_PEN);
+                this.runtime.renderer.setDrawableOrder(this._penDrawableId, Infinity, StageLayering.PEN_LAYER);
+            } else if (args.OPTION === LayerParam.BACK) {
+                console.log('setting the layer order to', StageLayering.LAYER_GROUPS)
+                this.runtime.renderer.setLayerGroupOrdering(StageLayering.LAYER_GROUPS);
+                this.runtime.renderer.setDrawableOrder(this._penDrawableId, -1, StageLayering.PEN_LAYER);
+            }
+        }
+    }
+
 }
 
 module.exports = Scratch3PenBlocks;
